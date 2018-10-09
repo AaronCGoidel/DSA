cddf9b29ec203bdacab3b4a0b11ad38de8b99b34
AaronCGoidel <agoidel2019@mka.org>
2018-10-09 19:32:00 -0400

    output code

--- Labs/Lab2.1/AlgoAnalysisHelper.cpp
+++ Labs/Lab2.1/AlgoAnalysisHelper.cpp
@@ -3,15 +3,18 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include "PrimeAlgos.h"
 
 using namespace std;
+
+typedef bool prime_fn(int num);
 
 // method that gets the arrays from a .txt file with
 // comma seperated values where a new line starts
 // a new list
-vector<vector<int>> getArrays(string fileName){
+vector<vector<int> > getArrays(string fileName){
   // create a vector of vectors of ints to store the integers
-  vector<vector<int>> data;
+  vector<vector<int> > data;
 
   // initialize the input stream and open the file
   ifstream infile;
@@ -65,14 +68,14 @@ void code_to_time(){
 
 
 //method to measure time of code_to_time()
-double measureTime(){
+double measureTime(prime_fn fn, int num){
   using namespace std::chrono;
 
   // create a start time
   steady_clock::time_point start = steady_clock::now();
 
   // call whatever function you are timing
-  code_to_time();
+  (*fn)(num);
 
   // get the end time
   steady_clock::time_point end = steady_clock::now();
@@ -87,17 +90,42 @@ double measureTime(){
 void output(vector<double> data, string filename){
   // take in the vector of doubles to output and desired filename
   // create a file with the given filename
-  ofstream myfile (filename);
-
+  ofstream outfile;
+  outfile.open(filename, std::ios_base::app);
   // iterate over the vector, outputting the data to the file
   for (int i = 0; i < data.size()-1; i++){
-    myfile << data[i] << ",";
+    outfile << data[i] << ",";
   }
-  myfile << data[data.size()-1] << endl;
+  outfile << data[data.size()-1] << endl;
 
   // close the file
-  myfile.close();
+  outfile.close();
 }
 
-  
-  
+int main(){
+  prime_fn* a = &one;
+  prime_fn* b = &two;
+  prime_fn* c = &three;
+
+  vector<vector<double> > times(3);
+  vector<vector<int> > nums = getArrays("primes.txt");
+
+  for(int i = 0; i < nums.size(); i++){
+    for(int j = 0; j < nums[i].size(); j++){
+      int num = nums[i][j];
+      times[0].push_back(measureTime(a, num));
+      times[1].push_back(measureTime(b, num));
+      times[2].push_back(measureTime(c, num));
+    }
+    output(times[0], "ONE.csv");
+    times[0].clear();
+
+    output(times[1], "TWO.csv");
+    times[1].clear();
+
+    output(times[2], "THREE.csv");
+    times[2].clear();
+  }
+
+  return 0;
+}
---
+++ Labs/Lab2.1/PrimeAlgos.h
@@ -0,0 +1,9 @@
+#ifndef PRIMEALGOS_H
+#define PRIMEALGOS_H
+
+bool one(int num);
+bool two(int num);
+bool three(int num);
+
+int modPow(int base, int power, int p);
+#endif
