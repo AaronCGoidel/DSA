2292950e3d395a03f4eb533fe36c35b21c9ee177
AaronCGoidel <agoidel2019@mka.org>
2018-10-09 17:45:34 -0400

    added Fermat test

--- Labs/Lab2.1/PrimeCheck.cpp
+++ Labs/Lab2.1/PrimeCheck.cpp
@@ -1,9 +1,14 @@
 #include <assert.h>
-#include <vector>
+#include <stdlib.h>
 #include <cmath>
+#include <iostream>
+
 using namespace std;
 
 bool one(int num){
+  if(num <= 1) return false;
+  if(num <= 3) return true;
+  
   for(int i = 2; i < round(sqrt(num)); i++){
     if(num % i == 0) return false;
   }
@@ -11,7 +16,8 @@ bool one(int num){
 }
 
 bool two(int num){
-  if(num == 2 || num == 3) return false;
+  if(num <= 1) return false;
+  if(num <= 3) return true;
   if(num % 2 == 0 || num % 3 == 0) return false;
 
   int i = 5;
@@ -25,3 +31,28 @@ bool two(int num){
   return true;
 }
 
+int modPow(int base, int power, int p){
+	int solution = 1;
+	while(power > 0){ 
+		if(power%2 != 0){
+			solution = (solution * base)%p;
+		}
+		power /= 2;
+		base = (base * base)%p;
+	}
+	return solution;
+}
+
+bool three(int num){
+  srand(time(NULL));
+
+  if(num == 1) return false;
+  
+  int iters = 5;
+  for(int i = 0; i < iters; i++){
+    int a = rand()%(num - 1) + 1;
+    
+    if(modPow(a, num - 1, num) != 1) return false;
+  }
+  return true;
+}
